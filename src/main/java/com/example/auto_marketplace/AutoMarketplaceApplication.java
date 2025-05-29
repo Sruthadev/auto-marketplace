a3d5b80fd648277f9b2fd4dc90b2fba7d4052dc8
sruthadev <sruthadev@gmail.com>
2025-05-29 23:26:20 +0530

    Initial commit

---
+++ src/main/java/com/example/auto_marketplace/AutoMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.auto_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/auto_marketplace/AutoMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auto_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
